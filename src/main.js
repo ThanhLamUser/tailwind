b09434656d9503a3f87523fd54402832d5c5f49e
ThanhLamUser <vanlamhv5@gmail.com>
2024-09-16 00:17:09 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#menu');
+const menuButton =document.querySelector('#menu-button');
+
+menuButton.addEventListener("click",()=>{
+    menu.classList.toggle("invisible");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height:{
+        custom:"620px"
+      },
+      spacing:{
+        "40-c":"40rem"
+      }
+    },
+  },
+  plugins: [],
+}
